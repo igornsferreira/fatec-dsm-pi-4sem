eab0e0efdccf1b31bfd6a58f1a10984388df850a
Igor Ferreira <igornsferreira@gmail.com>
2024-09-21 16:30:20 -0300

    [feat] melhorias no models.py (created_at, updated_at, etc)

--- sportsync/sportsync_app/models.py
+++ sportsync/sportsync_app/models.py
@@ -2,7 +2,7 @@ import uuid
 from django.db import models
 
 class Endereco(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id_endereco = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     cep = models.CharField(max_length=10)
     rua = models.CharField(max_length=255)
     bairro = models.CharField(max_length=255)
@@ -10,52 +10,75 @@ class Endereco(models.Model):
     cidade = models.CharField(max_length=255)
     estado = models.CharField(max_length=255)
     pais = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.rua}, {self.numero}, {self.cidade}, {self.estado}"
 
 class Usuario(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    ROLE_CHOICES = [
+        ('atleta', 'Atleta'),
+        ('organizador', 'Organizador'),
+    ]
+    
+    id_usuario = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nome = models.CharField(max_length=255)
-    senha_hash = models.CharField(max_length=255)
+    senha = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     cpf = models.CharField(max_length=11, unique=True)
     telefone = models.CharField(max_length=20)
     data_nascimento = models.DateField()
     endereco = models.ForeignKey(Endereco, on_delete=models.SET_NULL, null=True)
+    papel = models.CharField(max_length=12, choices=ROLE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nome
 
 class Esporte(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id_esporte = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nome = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nome
 
 class Evento(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id_evento = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nome = models.CharField(max_length=255)
     descricao = models.TextField()
+    organizador = models.ForeignKey(Usuario, on_delete=models.CASCADE, null=True)
     data_inicio = models.DateTimeField()
     data_fim = models.DateTimeField()
     max_participantes = models.IntegerField()
     esporte = models.ForeignKey(Esporte, on_delete=models.CASCADE)
+    endereco = models.ForeignKey(Endereco, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nome
 
 class Partida(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id_partida = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    evento = models.ForeignKey(Evento, on_delete=models.CASCADE, null=True)
     data_inicio = models.DateTimeField()
     data_fim = models.DateTimeField()
-    esporte = models.ForeignKey(Esporte, on_delete=models.CASCADE)
+    endereco = models.ForeignKey(Endereco, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Quadra(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id_quadra = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     nome = models.CharField(max_length=255)
-    evento = models.ForeignKey(Evento, on_delete=models.CASCADE)
+    endereco = models.ForeignKey(Endereco, on_delete=models.SET_NULL, null=True)
+    esporte = models.ForeignKey(Esporte, on_delete=models.CASCADE)
+    disponibilidade = models.TextField()  
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nome
