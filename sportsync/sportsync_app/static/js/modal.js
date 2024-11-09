9e3f2fd5f304f9e831d6643f38168d60315cccc1
Igor Ferreira <igornsferreira@gmail.com>
2024-11-09 17:44:54 -0300

    [update] ajustes modal

--- sportsync/sportsync_app/static/js/modal.js
+++ sportsync/sportsync_app/static/js/modal.js
@@ -2,31 +2,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('button[data-bs-toggle="modal"]');
     buttons.forEach(button => {
         button.addEventListener('click', function () {
+            // Pega o id e o nome da quadra do botão
             const quadraId = this.getAttribute('data-quadra-id');
             const nomeQuadra = this.getAttribute('data-nome-quadra');
+            
+            // Atribui os valores ao modal
             document.getElementById('quadraId').value = quadraId;
-            setLocal(nomeQuadra);
+            document.getElementById('local').value = nomeQuadra;
         });
     });
-
-    const form = document.getElementById('agendamentoForm');
-    if (form) {
-        form.addEventListener('submit', function (event) {
-            event.preventDefault();
-            alert('Agendamento confirmado!');
-
-            const modalElement = document.getElementById('agendamentoModal');
-            const modalInstance = bootstrap.Modal.getInstance(modalElement);
-            if (modalInstance) {
-                modalInstance.hide();
-            }
-        });
-    }
 });
-
-function setLocal(nomeQuadra) {
-    const localField = document.getElementById('local');
-    if (localField) {
-        localField.value = nomeQuadra;
-    }
-}
